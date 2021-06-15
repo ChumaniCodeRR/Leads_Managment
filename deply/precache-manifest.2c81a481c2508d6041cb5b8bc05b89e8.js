fbcd9e3b805ae39fb4c34af209fa40da92068515
TumishoMothobekhi <tumisho@vetro.co.za>
2021-06-15 15:19:10 +0200

    Deploy files

---
+++ deply/precache-manifest.2c81a481c2508d6041cb5b8bc05b89e8.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1720c9633b5c10bdb3c7f0a6e3935ded",
+    "url": "/volt-react-dashboard/index.html"
+  },
+  {
+    "revision": "c109acaaa8908dfb56eb",
+    "url": "/volt-react-dashboard/static/css/2.1d5ec968.chunk.css"
+  },
+  {
+    "revision": "7feb87d6e591ea26bd96",
+    "url": "/volt-react-dashboard/static/css/main.f5583e05.chunk.css"
+  },
+  {
+    "revision": "c109acaaa8908dfb56eb",
+    "url": "/volt-react-dashboard/static/js/2.5dd34f18.chunk.js"
+  },
+  {
+    "revision": "7feb87d6e591ea26bd96",
+    "url": "/volt-react-dashboard/static/js/main.18600083.chunk.js"
+  },
+  {
+    "revision": "753edd65a92408b593eb",
+    "url": "/volt-react-dashboard/static/js/runtime-main.0f4f30a9.js"
+  },
+  {
+    "revision": "85064a97348166467bd7bc2ad80ccdce",
+    "url": "/volt-react-dashboard/static/media/404.85064a97.svg"
+  },
+  {
+    "revision": "9ba4b7cbe7f072ea0fe50ec0d4b6c97b",
+    "url": "/volt-react-dashboard/static/media/404.9ba4b7cb.jpg"
+  },
+  {
+    "revision": "192d8f70e8dbebb4b03f98db1a6434c7",
+    "url": "/volt-react-dashboard/static/media/500.192d8f70.jpg"
+  },
+  {
+    "revision": "a0320da1a9ae65d73f6af7780883975c",
+    "url": "/volt-react-dashboard/static/media/500.a0320da1.svg"
+  },
+  {
+    "revision": "74404fd7e3f9660dfa5cae9f99fc5bda",
+    "url": "/volt-react-dashboard/static/media/Lead_System.74404fd7.gif"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/volt-react-dashboard/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "95abb40d53edf7f917949e8757cdf5c6",
+    "url": "/volt-react-dashboard/static/media/bs5-illustrations.95abb40d.svg"
+  },
+  {
+    "revision": "5d08ccab9059d0350d3777b7a708c3b8",
+    "url": "/volt-react-dashboard/static/media/canada.5d08ccab.svg"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/volt-react-dashboard/static/media/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/volt-react-dashboard/static/media/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/volt-react-dashboard/static/media/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/volt-react-dashboard/static/media/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/volt-react-dashboard/static/media/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/volt-react-dashboard/static/media/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/volt-react-dashboard/static/media/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/volt-react-dashboard/static/media/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/volt-react-dashboard/static/media/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/volt-react-dashboard/static/media/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/volt-react-dashboard/static/media/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/volt-react-dashboard/static/media/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/volt-react-dashboard/static/media/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/volt-react-dashboard/static/media/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/volt-react-dashboard/static/media/fa-solid-900.9ae050d1.woff2"
+  },
+  {
+    "revision": "935e70467bab0575ad62110dfae45476",
+    "url": "/volt-react-dashboard/static/media/forgot-password.935e7046.jpg"
+  },
+  {
+    "revision": "8b296b786a6ac07e0db19819ecda25bd",
+    "url": "/volt-react-dashboard/static/media/france.8b296b78.svg"
+  },
+  {
+    "revision": "5c9661f66b30896a0667a78ea18f2847",
+    "url": "/volt-react-dashboard/static/media/germany.5c9661f6.svg"
+  },
+  {
+    "revision": "f04de15f5111b19af062e0e7f2830950",
+    "url": "/volt-react-dashboard/static/media/italy.f04de15f.svg"
+  },
+  {
+    "revision": "f8dc0f044c962d058420eb35d2228f83",
+    "url": "/volt-react-dashboard/static/media/japan.f8dc0f04.svg"
+  },
+  {
+    "revision": "69f2db42fdfeb91a54fe1573b6b102c9",
+    "url": "/volt-react-dashboard/static/media/lock.69f2db42.jpg"
+  },
+  {
+    "revision": "8f76161cf83f8d4a9c427392f4164d33",
+    "url": "/volt-react-dashboard/static/media/marker.8f76161c.svg"
+  },
+  {
+    "revision": "b2d9831a6290a67a05fa6d65bb67d226",
+    "url": "/volt-react-dashboard/static/media/mockup-calendar-presentation.b2d9831a.png"
+  },
+  {
+    "revision": "fc6d4c754fd2b26a2e4775bc91b61bbc",
+    "url": "/volt-react-dashboard/static/media/mockup-map-presentation.fc6d4c75.png"
+  },
+  {
+    "revision": "77a4c6a89e893405dd125243971c3d61",
+    "url": "/volt-react-dashboard/static/media/mockup-presentation.77a4c6a8.png"
+  },
+  {
+    "revision": "00a526136ebe6ca9adbd18a0dcb9064d",
+    "url": "/volt-react-dashboard/static/media/overview.00a52613.jpg"
+  },
+  {
+    "revision": "9992e0ee061c844b089dd94784cf7e9d",
+    "url": "/volt-react-dashboard/static/media/profile-cover.9992e0ee.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/volt-react-dashboard/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/volt-react-dashboard/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/volt-react-dashboard/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/volt-react-dashboard/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/volt-react-dashboard/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/volt-react-dashboard/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "5e51d949230c7a92d64c73604e5a3243",
+    "url": "/volt-react-dashboard/static/media/react-logo-primary.5e51d949.svg"
+  },
+  {
+    "revision": "f7d45c01a0efd126cc1030d5cf78fd84",
+    "url": "/volt-react-dashboard/static/media/react-logo-transparent.f7d45c01.svg"
+  },
+  {
+    "revision": "b779e4fd2515e28f0233a071f3d46104",
+    "url": "/volt-react-dashboard/static/media/react-logo.b779e4fd.svg"
+  },
+  {
+    "revision": "99d034b6cea27baae946ce783434954f",
+    "url": "/volt-react-dashboard/static/media/react-mockup.99d034b6.png"
+  },
+  {
+    "revision": "0a4cc9cd55c1c298542e559d34622a48",
+    "url": "/volt-react-dashboard/static/media/reset-password.0a4cc9cd.jpg"
+  },
+  {
+    "revision": "43fe41840c256623201cc14f63415614",
+    "url": "/volt-react-dashboard/static/media/settings.43fe4184.jpg"
+  },
+  {
+    "revision": "ea051702c568e9d0199b6ddc1a52fff4",
+    "url": "/volt-react-dashboard/static/media/sign-in.ea051702.jpg"
+  },
+  {
+    "revision": "6774df144d28e4f7c55c1f7725823465",
+    "url": "/volt-react-dashboard/static/media/sign-up.6774df14.jpg"
+  },
+  {
+    "revision": "58ee1d5a0a93d00df3f6c12170ebd3ff",
+    "url": "/volt-react-dashboard/static/media/signin.58ee1d5a.svg"
+  },
+  {
+    "revision": "bbd344a3e9c39621e69a64c02e4b11de",
+    "url": "/volt-react-dashboard/static/media/themesberg-logo.bbd344a3.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/volt-react-dashboard/static/media/themesberg.105e5059.svg"
+  },
+  {
+    "revision": "b01a62bd6a7a6191b6a1a79bf85885fc",
+    "url": "/volt-react-dashboard/static/media/time-and-money.b01a62bd.gif"
+  },
+  {
+    "revision": "b98aee40114ed3eaae525ce4ec63b0c3",
+    "url": "/volt-react-dashboard/static/media/transactions.b98aee40.jpg"
+  },
+  {
+    "revision": "db68882cbf4beac3f3ee3923f1a91b82",
+    "url": "/volt-react-dashboard/static/media/united-kingdom.db68882c.svg"
+  },
+  {
+    "revision": "936feee6e8061e6bf4006dce278384dd",
+    "url": "/volt-react-dashboard/static/media/united-states-of-america.936feee6.svg"
+  }
+]);
